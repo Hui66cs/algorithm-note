d3aba9d79268c822ca683c10faec58c0c547914e
Henghui Zhu <2193638829@qq.com>
2026-01-27 15:14:00 +0800

    delete no use

--- main.cpp
+++ main.cpp
@@ -1,51 +1,99 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
 #include <queue>
 using namespace std;
-struct node{
-    int value;
-    node* next;
-    node* pre;
-    node(int n){
-        value=n;
-        pre=nullptr;
-        next=nullptr;
+
+void BFS(vector<int>& dist,vector<int>& parent,const vector<vector<int>>& adj,int s){
+    dist[s]=0;
+    queue<int> Q;
+    Q.push(s);
+    while(!Q.empty()){
+        int u=Q.front();
+        Q.pop();
+        int n=adj[u].size();
+        for(int i=0;i<n;i++){
+            int v=adj[u][i];
+            if(dist[v]==-1){
+                Q.push(v);
+                dist[v]=dist[u]+1;
+                parent[v]=u;
+            }
+        }
     }
-};
+}
+
+void Issafe(const vector<vector<int>>& adj,vector<bool>& safe,const vector<int>& dist){
+    int n=safe.size();
+    for(int i=1;i<n;i++){
+        if(dist[i]!=-1){
+            int m=adj[i].size();
+            safe[i]=true;
+            for(int j=0;j<m;j++){
+                if(dist[adj[i][j]]==-1){
+                    safe[i]=false;
+                    break;
+                }
+            }
+        }else{
+            safe[i]=false;
+        }
+    }
+}
+
+void pro_BFS(vector<int>& dist,vector<int>& parent,const vector<bool>& safe,const vector<vector<int>>& adj,int s){
+    if(!safe[s]){
+        return;
+    }
+    dist[s]=0;
+    queue<int> Q;
+    Q.push(s);
+    while(!Q.empty()){
+        int u=Q.front();
+        Q.pop();
+        int n=adj[u].size();
+        for(int i=0;i<n;i++){
+            int v=adj[u][i];
+            if(dist[v]==-1&&safe[v]){
+                Q.push(v);
+                dist[v]=dist[u]+1;
+                parent[v]=u;
+            }
+        }
+    }
+}
+
+void Show_Shortest_Path(const vector<int>& parent,int path_node){
+    if(parent[path_node]==-1){
+        cout<<path_node<<" ";
+        return;
+    }
+    Show_Shortest_Path(parent,parent[path_node]);
+    cout<<path_node<<" ";
+}
+
 int main(){
-    int n;
-    int m;
-    cin>>n;
-    cin>>m;
-    vector<node*> arr(n+1);
-    for(int i=1;i<=n;i++){
-        arr[i]=new node(i);
+    int V;
+    int E;
+    cin>>V>>E;
+    vector<vector<int>> adj(V+1);
+    vector<vector<int>> rev_adj(V+1);
+    for(int i=0;i<E;i++){
+        int u;
+        int v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        rev_adj[v].push_back(u);
     }
-    for(int i=2;i<n;i++){
-        arr[i]->next=arr[i+1];
-        arr[i]->pre=arr[i-1];
-    }
-    arr[n]->next=arr[1];
-    arr[n]->pre=arr[n-1];
-    arr[1]->next=arr[2];
-    arr[1]->pre=arr[n];
-    int record=1;
-    while(n!=0){
-        if(n==1){
-           cout<<arr[record]->value;
-           delete arr[record];
-           break;
-        }
-        node* temp=arr[record];
-        for(int i=1;i<m;i++){
-            temp=temp->next;
-        }
-        temp->pre->next=temp->next;
-        temp->next->pre=temp->pre;
-        cout<<temp->value<<" ";
-        record=temp->next->value;
-        delete temp;
-        n--;
-    }
+    int source;
+    int dest;
+    cin>>source>>dest;
+    vector<int> dist1(V+1,-1);
+    vector<int> parent1(V+1,-1);
+    vector<int> dist2(V+1,-1);
+    vector<int> parent2(V+1,-1);
+    BFS(dist2,parent2,rev_adj,dest);
+    vector<bool> safe(V+1);
+    Issafe(adj,safe,dist2);
+    pro_BFS(dist1,parent1,safe,adj,source);
+    cout<<dist1[dest]<<endl;
 }
